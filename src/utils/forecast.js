d16d601d205670cafe4b0f12ee9770014e6370de
István Szécsi <szecsi.istvan@gmail.com>
2019-07-05 12:09:07 +0200

    Replaced hungarian text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (longitude, latitude, callback) => {
-  const url = 'https://api.darksky.net/forecast/c749d96170a9845ed5f3e63112613a9c/' + longitude + ',' + latitude + '?units=si&lang=hu';
+  const url = 'https://api.darksky.net/forecast/c749d96170a9845ed5f3e63112613a9c/' + longitude + ',' + latitude + '?units=si';
   request({url, json: true}, (error, {body}) => {
     if (error) {
       callback('Unable to connect to location services', undefined);
@@ -15,13 +15,3 @@ const forecast = (longitude, latitude, callback) => {
 
 module.exports = forecast;
 
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
